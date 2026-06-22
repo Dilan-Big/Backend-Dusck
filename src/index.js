ea807cd14861f64a628be5fc715590e1734d0427
Dilan Gonzalez <dilanbigddata@gmail.com>
2026-06-22 18:17:32 -0500

    Servidor en funcionamiento

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/health',(req, res)=>{
+    res.json({
+        msg:'Servidor en funcionamiento'
+    });
+});
+
 app.listen(3000, ()=>{
     console.log ("Servidor lansado en http://localhost:3000")
 });
