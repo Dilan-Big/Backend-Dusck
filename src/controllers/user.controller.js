1f4eb5777f22b611f9f9c8a79f6c9a951250018a
Dilan Gonzalez <dilanbigddata@gmail.com>
2026-06-24 18:11:15 -0500

    Se registra Trycatch de usuarios y listados de usuarios

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,24 +1,40 @@
 import { dbCreateUser, dbGetUsers } from "../services/user.service.js";
 
 const createUser = async (req, res) => {
-    const inputData = req.body
+
+    try {
+        const inputData = req.body
 
 
-    const data = await dbCreateUser(inputData)
+        const data = await dbCreateUser(inputData)
 
-    res.json({
-        msg: "Se registra un usuario",
-        data
-    });
+        res.json({
+            msg: "Se registra un usuario",
+            data
+        });
+    } catch (error) {
+        res.json({
+            msg: "Ocurrio un error al obtener el usuario"
+        })
+    }
+
+
 }
 
 const getUser = async (req, res) => {
-       
+    try {
         const data = await dbGetUsers()
         res.json({
             msg: "Se obtiene el listado de usuarios",
             data
         });
-    };
+    } catch (error) {
+        console.error(error)
+        res.json({
+            msg: "Ocurrio un error a obtener la lista de usuarios"
+        })
+    }
+
+};
 
 export { createUser, getUser };
