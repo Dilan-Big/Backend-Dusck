21fdd31ec88bb308c71f2eb193080438797e8312
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-06-26 20:24:42 -0500

    se crea getCategoryById por id las rutas

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,9 +1,10 @@
 import { Router  } from "express";
-import { createCategory, getCategory } from "../controllers/category.controllers.js";
+import { createCategory, getCategory, getCategoryById } from "../controllers/category.controllers.js";
 
 const router = Router();
 
 router.post('/', createCategory);
 router.get('/', getCategory);
+router.get('/',getCategoryById);
 
 export default router;
