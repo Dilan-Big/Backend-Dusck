5afda4a7d319fa68beeed95ecc016c6c65c9258b
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-06-26 20:46:06 -0500

    se crea updateCategory por id las rutas

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,10 +1,11 @@
 import { Router  } from "express";
-import { createCategory, getCategory, getCategoryById } from "../controllers/category.controllers.js";
+import { createCategory, getCategory, getCategoryById, updateCategoryById } from "../controllers/category.controllers.js";
 
 const router = Router();
 
 router.post('/', createCategory);
 router.get('/', getCategory);
-router.get('/',getCategoryById);
+router.get('/:id',getCategoryById);
+router.patch('/:id', updateCategoryById);
 
 export default router;
