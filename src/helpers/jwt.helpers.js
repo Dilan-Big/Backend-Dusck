66a843918a742a09bdd8b93cc86803f805281612
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-07-01 18:30:14 -0500

    se hizo la funcion de renovacion token

--- src/helpers/jwt.helpers.js
+++ src/helpers/jwt.helpers.js
@@ -1,9 +1,20 @@
 import jwt from "jsonwebtoken"
 
 const generarToken = (payload) => {
-    return jwt.sign(payload, 'jhvyt76',{expiresIn:'1h'} )
+    return jwt.sign(payload, 'jhvyt76',{expiresIn:'1h'} );
+}
+
+const validateToken = (token) => {
+    try {
+         return jwt.verify(token, 'jhvyt76');
+    } catch (error) {
+        console.error(error)
+        return null 
+    }
+   
 }
    
 export {
-    generarToken
+    generarToken,
+    validateToken
 }
