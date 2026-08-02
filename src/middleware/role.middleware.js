07953a89fa27172045db29c9319481424f8c91f3
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-08-01 23:51:13 -0500

    se crea el de usuario y de category sea para l autenticacion y admin

--- src/middleware/role.middleware.js
+++ src/middleware/role.middleware.js
@@ -6,7 +6,22 @@ const isAdmin = (req , res, next) => {
             msg:"No tienes permisos para realizar esta acción"
         });
     }
-    next()
+    next();
 }
 
-export default isAdmin;
+const verifyUserPermission = (req, res, next) => {
+    const idFromUrl = req.params.id;                    //Usuario pueda editar su propio perfil
+    const idFromToken = req.user._id.toString();        //No puede editar cualquier perfil solo el de el ("toString" convierte a texto para comparar bien)
+
+    const esElMismoUsuario  = idFromUrl === idFromToken;
+    const esAdmin = req.user.role === 'administrador';  //admin puede editar cualquiere perfil
+
+      if (! (esElMismoUsuario || esAdmin) ) {
+        return res.status(403).json({
+            msg: "No tienes permisos para modificar este usuario"
+        });
+    }
+    next();
+}
+
+export  {isAdmin, verifyUserPermission};
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createProduct, deleteProductById, getProduct, getProductById, updateProductById } from "../controllers/product.controllers.js"
 import autenticationUser from "../middleware/autentication.middleware.js";
-import isAdmin from "../middleware/role.middleware.js";
+import { isAdmin } from "../middleware/role.middleware.js";
 
 const router = Router();
 
 router.post('/', autenticationUser, isAdmin, createProduct);
-router.get('/', autenticationUser, getProduct);
-router.get('/:id', autenticationUser, getProductById);
+router.get('/', getProduct);
+router.get('/:id', getProductById);
 router.patch('/:id',autenticationUser, isAdmin, updateProductById);
 router.delete('/:id',autenticationUser, isAdmin,  deleteProductById)
 
