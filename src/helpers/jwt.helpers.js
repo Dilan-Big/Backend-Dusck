63bde308a757c233ae47966bb58a627e8963a966
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-08-03 20:20:34 -0500

    ultimo commit

--- src/helpers/jwt.helpers.js
+++ src/helpers/jwt.helpers.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generarToken = (payload) => {
-  return jwt.sign(payload, "REDACTED_HISTORICAL_JWT_SECRET", { expiresIn: "1h" });
+  return jwt.sign(payload, "REDACTED_HISTORICAL_JWT_SECRET", { expiresIn: "7d" });
 };
 
 const validateToken = (token) => {
