f9cd9edc38e77f84a07688c8ebd56a271362967d
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-08-10 21:08:59 -0500

    se crea una funcionalidad que intenta tener un carrito que no existe y en consecuencia lo crea

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,37 +1,14 @@
 import CartModel from "../models/cart.model.js";
 
-const dbCreateCart = async (cart) => {
-    return await CartModel.create(cart);
-};
-
-const dbGetCarts = async () => {
-    return await CartModel.find()
-        .populate("userId")
-        .populate("products.productId");
-};
-
-const dbGetCartById = async (id) => {
-    return await CartModel.findById(id)
-        .populate("userId")
-        .populate("products.productId");
-};
-
-const dbUpdateCartById = async (id, cartUpdate) => {
-    return await CartModel.findByIdAndUpdate(
-        id,
-        cartUpdate,
-        { new: true }
-    );
-};
-
-const dbDeleteCartById = async (id) => {
-    return await CartModel.findByIdAndDelete(id);
-};
+const dbGetCart = async (userId) => {
+   return CartModel.findOneAndUpdate(
+    { userId },                                 // Objeto de consulta
+    { $setOnInsert: { userId, items: [] } },    // Datos a actualizar
+    { returnDocument: 'after', upsert: true, runValidators: true }
+   );
+    
+}
 
 export {
-    dbCreateCart,
-    dbGetCarts,
-    dbGetCartById,
-    dbUpdateCartById,
-    dbDeleteCartById
-};
+    dbGetCart
+}
